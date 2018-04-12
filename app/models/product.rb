3d7b2cadde37c6e87a78d1531cc444f859d0b7d4
s-gubernator <svitlana.gubernator@yahoo.com>
2018-04-12 23:45:21 +0300

    Added Cart and LineItem

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ApplicationRecord
+	has_many :line_items 
+  before_destroy :ensure_not_referenced_by_any_line_item
+
 	REQUIRED_FIELDS = [:title, :description, :image_url]
 
 	validates *REQUIRED_FIELDS, presence: true
@@ -9,4 +12,13 @@ class Product < ApplicationRecord
   with:   %r{\.(gif|jpg|png)\Z}i,
   message: 'must be a URL for GIF, JPG or PNG image.'
     }
+
+  private 
+    # ensure that there are no line items referencing this product
+    def ensure_not_referenced_by_any_line_item
+      unless line_items.empty?
+        errors.add(:base, 'Line Items present')
+        throw :abort
+      end
+    end
 end
