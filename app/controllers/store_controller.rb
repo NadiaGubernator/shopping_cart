dfa3b25cd13091d2d2bf82df834d7e6082aedd03
s-gubernator <svitlana.gubernator@yahoo.com>
2018-04-15 15:44:03 +0300

    Minor changes to store

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,13 @@
 class StoreController < ApplicationController
   def index
+  	@date = DateTime.now    
   	@products = Product.order(:title)
+  	initialize_session
+  	@session_counter_message = "You've been here #{session[:counter]} times" if session[:counter] > 5
   end
+
+  private
+    def initialize_session    	
+      session[:counter].nil? ? session[:counter] = 0 : session[:counter] += 1      
+    end
 end
