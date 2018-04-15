5a9ffd592a931827a57eb7753914db501787b093
s-gubernator <svitlana.gubernator@yahoo.com>
2018-04-15 18:39:37 +0300

    Added quantity to line items

--- README.md
+++ README.md
@@ -1,12 +1,11 @@
 # Online Store
-This application is based on *Agile Web Development with Rails*
+This application is based on *Agile Web Development with Rails*.
 
 ## Web store with catalog, cart, and orders.
 
 It is divided into two main sections:
 
-* The buyer's side of the application manages the catalog, cart, 
-  and checkout.
+* The buyer's side of the application manages the catalog, cart, and checkout.
 
 * Only administrators can access stuff in the seller's side
   (product maintenance and order fulfillment).
--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -26,12 +26,12 @@ class LineItemsController < ApplicationController
   # POST /line_items
   # POST /line_items.json
   def create
-    product = Product.find(params[:product_id])
-    @line_item = @cart.line_items.build(product: product)
+    product = Product.find(params[:product_id])    
+    @line_item = @cart.add_product(product)
    
     respond_to do |format|
       if @line_item.save
-        session[:counter] = nil
+        session[:counter] = 0
         format.html { redirect_to @line_item.cart, notice: 'Line item was successfully created.' }
         format.json { render :show, status: :created, location: @line_item }
       else
