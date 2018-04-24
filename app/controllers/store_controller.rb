3f450a3ae57254bfa6dd2c9fae722b9bc238d9fa
s-gubernator <svitlana.gubernator@yahoo.com>
2018-04-24 21:45:35 +0300

    Broadcasting product updates

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -2,15 +2,15 @@ class StoreController < ApplicationController
   include CurrentCart
   before_action :set_cart
   
-  def index
-  	@date = DateTime.now    
+  def index  	    
   	@products = Product.order(:title)
-  	initialize_session  	
+  	# initialize_session  	
   end
 
   private
-    def initialize_session    	
-      session[:counter].nil? ? session[:counter] = 0 : session[:counter] += 1 
-      @session_counter_message = "You've been here #{session[:counter]} times" if session[:counter] > 5     
-    end
+    # def initialize_session          	
+    #   session[:counter].nil? ? session[:counter] = 0 : session[:counter] += 1 
+    #   @session_counter_message = "You've been here #{session[:counter]} times" if session[:counter] > 5  
+    #   @date = DateTime.now   
+    # end
 end
