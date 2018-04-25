6fca600816ba85c86ff92f8c52a464ca51924ba7
s-gubernator <svitlana.gubernator@yahoo.com>
2018-04-25 13:48:31 +0300

    Added RedisToGo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,6 +43,8 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.web_socket_server_url = "wss://shopping-cart-s-gubernator.herokuapp.com/cable" 
+  config.action_cable.allowed_request_origins = ['https://shopping-cart-s-gubernator.herokuapp.com', 'http://shopping-cart-s-gubernator.herokuapp.com']
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true 
